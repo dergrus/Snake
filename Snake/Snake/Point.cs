833eea980c7acc6f6536ae8e3bb599f9b693204a
dergrus <dergrus@gmail.com>
2020-05-11 21:20:13 +0500

    Урок 4 конструктор

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -11,6 +11,19 @@ namespace Snake
         public int x;
         public int y;
         public char sym;
+
+        public Point(int _x,int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public Point()
+        {
+
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
